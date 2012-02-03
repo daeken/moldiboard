821bd920a2b7951ba142f90c5e96bf914f4b3174
Cody Brocious <cody.brocious@gmail.com>
2012-02-02 19:51:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moldiboard::Application.config.secret_token = 'cdde948ee306805d0f1bfa6f7a07fd7e11299311a29cfd566ce464bda544a622769b2a8e582c2c1834b90d5528f5e943255b599291697db4979c864d3e88e2ae'
